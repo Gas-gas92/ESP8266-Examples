d5b7fbccbe62be309aa6bc73b3269abf237a430a
GasGas92 <103871856+Gas-gas92@users.noreply.github.com>
2023-09-04 02:31:51 +0200

    Update config.h

--- 00_Connect_Wifi_STA/config.h
+++ 00_Connect_Wifi_STA/config.h
@@ -1,7 +1,7 @@
-const char* ssid     = "ssid";
-const char* password = "password";
+const char* ssid     = "MOVISTAR_ED7A ";
+const char* password = "qbXuRh4Gt7j5fMNeB7MK";
 const char* hostname = "ESP8266_1";
 
-IPAddress ip(192, 168, 1, 200);
+IPAddress ip(192, 168, 1, 176);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
